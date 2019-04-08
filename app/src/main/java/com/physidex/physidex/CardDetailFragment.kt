7424e9ac378c5cc855753073869ba081a6c75db4
Nick Garvey <garveyness03@gmail.com>
2019-04-07 20:24:51 -0400

    Worked on CardDetail

--- app/src/main/java/com/physidex/physidex/CardDetailFragment.kt
+++ app/src/main/java/com/physidex/physidex/CardDetailFragment.kt
@@ -1,12 +1,11 @@
 package com.physidex.physidex
 
+import android.content.Context
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
-import android.widget.EditText
-import android.widget.ImageView
+import android.widget.*
 import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
@@ -20,6 +19,9 @@ class CardDetailFragment : Fragment(), View.OnClickListener {
 
     lateinit var detailedCard: FullPokeCard
     private lateinit var cardViewModel: PokeCardViewModel
+    lateinit var linearLayout: LinearLayout
+
+
 
     override fun onCreateView(
             inflater: LayoutInflater,
@@ -29,7 +31,7 @@ class CardDetailFragment : Fragment(), View.OnClickListener {
         val view = inflater.inflate(R.layout.card_detail, container, false)
 
         // Set up view model (used if added to binder)
-        cardViewModel = ViewModelProviders.of(this).get(PokeCardViewModel::class.java)
+        //cardViewModel = ViewModelProviders.of(this).get(PokeCardViewModel::class.java)
 
         // add to binder
         val button: Button = view.findViewById(R.id.add_button)
@@ -43,6 +45,11 @@ class CardDetailFragment : Fragment(), View.OnClickListener {
                     .into(cardDetailImage)
 
             val info: Map<String, String> = detailedCard.getInfo()
+
+            // Load LinearLayout
+            linearLayout = view.findViewById(R.id.card_detail_linear_layout)
+            linearLayout.orientation = LinearLayout.VERTICAL
+
             this.fillTable(detailedCard, info)
         }
 
@@ -56,9 +63,19 @@ class CardDetailFragment : Fragment(), View.OnClickListener {
     fun fillTable(cardSelected: FullPokeCard, info: Map<String, String>) {
 
         //TODO: Fill table with info in map.
+        var rowView: View?
+        var nameField: TextView
+        var dataField: TextView
 
         for ((key, value) in info) {
+            rowView = this.layoutInflater.inflate(R.layout.card_detail_field, null)
+            nameField = rowView.findViewById(R.id.field_name)
+            dataField = rowView.findViewById(R.id.field_data)
 
+            nameField.text = key
+            dataField.text = value
+
+            linearLayout.addView(rowView)
         }
     }
 
